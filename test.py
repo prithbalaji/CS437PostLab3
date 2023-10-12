62ad192e3bd4900481c17c2ba5dc0e9743fd8242
Prithvi Balaji <prithvibalaji16@gmail.com>
2023-10-12 17:21:27 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+# Set the LED color (R, G, B)
+red = (255, 0, 0)
+
+# Set the X and Y coordinates of the LED you want to light up
+x = 3
+y = 3
+
+# Turn on the LED at the specified coordinates
+sense.set_pixel(x, y, red)
